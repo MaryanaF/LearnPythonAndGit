e04545e0b9e562554e357de04b2b024e6d051198
Maryana Fraga <fraga.azul.maryana@gmail.com>
2024-06-24 16:27:56 -0300

    Comentando algumas linhas de código

--- PineappleJuice.py
+++ PineappleJuice.py
@@ -1,12 +1,18 @@
 import pygame
 
 pygame.init()
+
+#   cria janela do jogo (tamanho)
+#                       (nome)
+#   inicia o relogio
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Pineapple Juice')
 clock = pygame.time.Clock()
 
 died = False
 
+#   para cada evento, mostra o evento
+#   se evento == clicar X, fecha o jogo
 while not died:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
